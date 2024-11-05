7552dc95cdb08d2a6d3a330339696af84ce191ba
HanYoonSoo <hdbstn3055@gmail.com>
2024-11-05 12:38:25 +0900

    init: Project Setting

---
+++ src/main/java/study/stomp/stompstudy/StompStudyApplication.java
@@ -0,0 +1,13 @@
+package study.stomp.stompstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompStudyApplication.class, args);
+    }
+
+}
